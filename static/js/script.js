d312d4af3b978c7c8d5b449cb6959da705d47e52
Domengius_20-11 <kiccofranca13@gmail.com>
2025-01-07 09:30:04 +0100

    lavoro completato

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+let background1 = document.querySelectorAll(".myElement");
+
+for(let i=0; i<background1.length; i++){
+    background1[i].addEventListener("click", function(event){
+        event.currentTarget.classList.remove("greenBg");
+        event.currentTarget.classList.add("yellowBg");
+    });
+}
