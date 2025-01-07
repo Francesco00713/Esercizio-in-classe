2958a555b7527a4d24806b3de040df3fc53c872a
Domengius_20-11 <kiccofranca13@gmail.com>
2025-01-07 09:33:21 +0100

    aggiunta transizione

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ let background1 = document.querySelectorAll(".myElement");
 for(let i=0; i<background1.length; i++){
     background1[i].addEventListener("click", function(event){
         event.currentTarget.classList.remove("greenBg");
+        event.currentTarget.classList.add("transition");
         event.currentTarget.classList.add("yellowBg");
     });
 }
